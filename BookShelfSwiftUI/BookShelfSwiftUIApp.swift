4eb539e784cb25ddbb1fd305ecac6d410c70a792
tanya_bond <bondarenkotatiana96@gmail.com>
2022-07-07 13:40:39 -0700

    Initial commit

---
+++ BookShelfSwiftUI/BookShelfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfSwiftUIApp.swift
+//  BookShelfSwiftUI
+//
+//  Created by Tatiana Bondarenko on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
